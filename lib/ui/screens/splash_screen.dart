a5bb374ba0f233034a9a234eecbb4923a2363d9c
mohsin <arifbsns22@gmail.com>
2025-05-08 16:28:49 +0600

    1st commit with splash screen progress

--- lib/ui/screens/splash_screen.dart
+++ lib/ui/screens/splash_screen.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
+import '../utills/assets_path.dart';
+
 class SplashScreen extends StatefulWidget {
   const SplashScreen({super.key});
 
@@ -12,7 +14,17 @@ class _SplashScreenState extends State<SplashScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body:SvgPicture.asset('assets/images/background.svg'),
-      );
+      body: Stack(
+        children: [
+          SvgPicture.asset(
+            width: double.maxFinite,
+            height: double.maxFinite,
+            AssetsPath.backgroundSvg,
+            fit: BoxFit.cover,
+          ),
+          Center(child: SvgPicture.asset(AssetsPath.logoSvg)),
+        ],
+      ),
+    );
   }
 }
